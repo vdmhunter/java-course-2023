8d662e6613afa0eada96cda8b570682e3eb67794
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-10-11 02:26:22 +0300

    hw1: Task0: Refactor Task0
    Refactor Task0 to use Log4j for output

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -1,18 +1,22 @@
 package edu.hw1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
- * The Task0 class provides a method {@link Task0#helloWord} which returns the string "Привет, мир!".
+ * The Task0 class provides a method {@link Task0#helloWord} which print the string "Привет, мир!".
  */
 public final class Task0 {
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
     private Task0() {
     }
 
     /**
-     * Return the string "Привет, мир!"
-     *
-     * @return The constant string "Привет, мир!"
+     * Print the string "Привет, мир!"
      */
-    public static String helloWord() {
-        return "Привет, мир!";
+    public static void helloWord() {
+        LOGGER.info("Привет, мир!");
     }
 }

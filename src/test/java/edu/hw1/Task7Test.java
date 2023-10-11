5f40b843d9e86ff03ed6bb29d13887d0d25f146c
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-10-11 03:30:51 +0300

    hw1: Task7: Refactor tests for Task7
    Refactor Task7 to accept binary representations of integer numbers as test input.

--- src/test/java/edu/hw1/Task7Test.java
+++ src/test/java/edu/hw1/Task7Test.java
@@ -9,33 +9,37 @@ import org.junit.jupiter.params.provider.CsvSource;
  * Tests for Homework 1, Task 7
  */
 public class Task7Test {
-    @ParameterizedTest(name = "Test {index} - rotateLeft: n={0}, shift={1}, expected={2}")
+    @ParameterizedTest(name = "Test {index} - rotateLeft: n=0b{0}, shift={1}, expected=0b{2}")
     @CsvSource({
-        "16,1,1",
-        "17,2,6",
-        "102,0,102",
-        "102,1,77",
-        "102,2,27",
-        "2147483647,10,2147483647",
+        "10000,1,00001",
+        "10001,2,00110",
+        "1100110,0,1100110",
+        "1100110,1,1001101",
+        "1100110,2,0011011",
+        "1111111111111111111111111111111,10,1111111111111111111111111111111",
     })
     @DisplayName("Happy paths for rotateLeft")
-    void rotateLeft_HappyPaths(int n, int shift, int expected) {
+    void rotateLeft_HappyPaths(String nStr, int shift, String expectedStr) {
+        int n = Integer.parseInt(nStr, 2);
+        int expected = Integer.parseInt(expectedStr, 2);
         int actual = Task7.rotateLeft(n, shift);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @ParameterizedTest(name = "Test {index} - rotateRight: n={0}, shift={1}, expected={2}")
+    @ParameterizedTest(name = "Test {index} - rotateRight: n=0b{0}, shift={1}, expected=0b{2}")
     @CsvSource({
-        "8,1,4",
-        "85,0,85",
-        "85,1,106",
-        "85,2,53",
-        "85,3,90",
-        "2147483647,10,2147483647",
+        "1000,1,0100",
+        "1010101,0,1010101",
+        "1010101,1,1101010",
+        "1010101,2,0110101",
+        "1010101,3,1011010",
+        "1111111111111111111111111111111,10,1111111111111111111111111111111",
     })
     @DisplayName("Happy paths for rotateRight")
-    void rotateRight_HappyPaths(int n, int shift, int expected) {
+    void rotateRight_HappyPaths(String nStr, int shift, String expectedStr) {
+        int n = Integer.parseInt(nStr, 2);
+        int expected = Integer.parseInt(expectedStr, 2);
         int actual = Task7.rotateRight(n, shift);
 
         Assertions.assertEquals(expected, actual);

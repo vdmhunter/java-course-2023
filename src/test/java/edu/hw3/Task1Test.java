11a97107087a2e27550bccc086747c1db22e95f7
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-10-23 17:47:06 +0300

    hw3: Add files for Task1
    Add Homework 3 Task1 files and tests for them.

---
+++ src/test/java/edu/hw3/Task1Test.java
@@ -0,0 +1,47 @@
+package edu.hw3;
+
+import edu.hw3.task1.Encoder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Tests for Homework 3, Task 1
+ */
+public class Task1Test {
+    @ParameterizedTest(name = "Test {index} - Encode \"{0}\" with Atbash should return \"{1}\"")
+    @Order(1)
+    @CsvSource({
+        "Hello world!,Svool dliow!",
+        "Any fool can write code that a computer can understand. " +
+            "Good programmers write code that humans can understand. ― Martin Fowler," +
+            "Zmb ullo xzm dirgv xlwv gszg z xlnkfgvi xzm fmwvihgzmw. " +
+            "Tllw kiltiznnvih dirgv xlwv gszg sfnzmh xzm fmwvihgzmw. ― Nzigrm Uldovi",
+        "Привет мир!,Привет мир!"
+    })
+    @DisplayName("Happy paths")
+    void atbash_HappyPaths(String str, String expected) {
+        // Act
+        String actual = Encoder.atbash(str);
+
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test when input string is null")
+    void atbash_InputStringIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> Encoder.atbash(null));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test when input string is empty")
+    void atbash_InputStringIsEmpty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Encoder.atbash(""));
+    }
+}

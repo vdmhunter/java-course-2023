91b7039199c7b7b5399fecad597a08e4e824a4dc
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-10-23 19:29:15 +0300

    hw3: Task1: Add the TestMethodOrder annotation
    Add the TestMethodOrder annotation to the Task1Test class

--- src/test/java/edu/hw3/Task1Test.java
+++ src/test/java/edu/hw3/Task1Test.java
@@ -3,14 +3,17 @@ package edu.hw3;
 import edu.hw3.task1.Encoder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * Tests for Homework 3, Task 1
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Task1Test {
     @ParameterizedTest(name = "Test {index} - Encode \"{0}\" with Atbash should return \"{1}\"")
     @Order(1)

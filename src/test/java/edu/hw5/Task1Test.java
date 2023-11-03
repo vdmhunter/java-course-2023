82ff6268a521e5a179e3564c98effbac2e34ec5e
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-11-03 19:27:28 +0300

    hw5: Add files for Task1
    Add Homework 5 Task1 files and tests for them.

---
+++ src/test/java/edu/hw5/Task1Test.java
@@ -0,0 +1,91 @@
+package edu.hw5;
+
+import edu.hw5.task1.ComputerClubAnalytics;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * Tests for Homework 5, Task 1
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Task1Test {
+    @ParameterizedTest(name = "Test {index} - Average session time is {1}")
+    @Order(1)
+    @MethodSource("provideListsForHappyPaths")
+    @DisplayName("Happy paths")
+    void getAverageTime_HappyPaths(List<String> list, String expected) {
+        // Act
+        String actual = ComputerClubAnalytics.getAverageTime(list);
+
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest(name = "Test {index} - Average session time for incorrect input is {1}")
+    @Order(2)
+    @MethodSource("provideListsForFailPaths")
+    @DisplayName("Fail paths")
+    void getAverageTime_FailPaths(List<String> list, String expected) {
+        // Act
+        String actual = ComputerClubAnalytics.getAverageTime(list);
+
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test when input list is null")
+    void getAverageTime_InputStringIsNull() {
+        Assertions.assertThrows(
+            NullPointerException.class,
+            () -> ComputerClubAnalytics.getAverageTime(null)
+        );
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test when input list is empty")
+    void getAverageTime_InputStringIsEmpty() {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> ComputerClubAnalytics.getAverageTime(new ArrayList<>())
+        );
+    }
+
+    private static Stream<Arguments> provideListsForHappyPaths() {
+        return Stream.of(
+            Arguments.of(new ArrayList<String>() {{
+                add("2022-03-12, 20:20 - 2022-03-12, 23:50");
+                add("2022-04-01, 21:30 - 2022-04-02, 01:20");
+            }}, "3h 40m"),
+            Arguments.of(new ArrayList<String>() {{
+                add("2023-10-01, 08:00 - 2023-10-03, 23:00");
+                add("2023-10-05, 15:21 - 2023-10-05, 16:49");
+                add("2023-10-06, 09:10 - 2023-10-07, 22:11");
+                add("2023-10-08, 13:34 - 2023-10-09, 12:38");
+            }}, "31h 8m")
+        );
+    }
+
+    private static Stream<Arguments> provideListsForFailPaths() {
+        return Stream.of(
+            Arguments.of(new ArrayList<String>() {{
+                add("");
+                add("abcdef");
+                add("2022-13-32, 25:77 - 2022-14-82, 31:66");
+                add("2022-54-71, 38:98 - 2022-84-72, 51:80");
+            }}, "0h 0m")
+        );
+    }
+}
